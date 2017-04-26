fa45dc55ec06d6caac54d82538d17255d1aaae08
Adhy <adhywiranatap@gmail.com>
2017-04-26 13:22:20 +0700

    props without redux

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <App users={[
+      { id: 1, name: 'Mike'},
+      { id: 2, name: 'John'},
+      { id: 3, name: 'Sam'},
+    ]} />,
   document.getElementById('root')
 );
