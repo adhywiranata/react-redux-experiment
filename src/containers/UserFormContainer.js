ae854ebe989875e1d0fa2c26525c7cf23f44a976
Adhy <adhywiranatap@gmail.com>
2017-05-01 22:23:22 +0700

    refactored styles

--- src/containers/UserFormContainer.js
+++ src/containers/UserFormContainer.js
@@ -4,35 +4,7 @@ import { connect } from 'react-redux';
 import { addUser } from '../actions';
 import FormSection from '../components/FormSection';
 
-const styles = {
-  addFormContainer: {
-    border: '1px solid rgba(0, 0, 0, .1)',
-    padding: 30,
-    backgroundColor: '#22313F',
-    color: '#FFFFFF',
-    height: '100vh',
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  addForm: {
-    flex: 1,
-    width: '50%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  submitBtn: {
-    border: 0,
-    padding: 20,
-    backgroundColor: '#446CB3',
-    color: '#FFFFFF',
-    margin: 5,
-    marginTop: 20,
-    fontSize: '1em',
-  },
-};
+import styles from './formStyles';
 
 const initialState = {
   name: '',
