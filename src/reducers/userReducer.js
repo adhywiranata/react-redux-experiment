820b1be8ea4719b24bdcc91784378d481ce040de
Adhy <adhywiranatap@gmail.com>
2017-05-01 23:23:36 +0700

    added failed fetching to user reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -9,22 +9,34 @@ const initialState = {
   users: [],
   isFetching: false,
   fetchError: false,
+  fetchErrorMessage: '',
+};
+
+const fetchUserStartLoading = (state) => {
+  return { ...state, isFetching: true }
+}
+
+const fetchUser = (state, payload) => {
+  return { ...state, users: payload, isFetching: false };
+};
+
+const fetchUserFailed = (state, payload) => {
+  return { ...state, isFetching: false, fetchError: true, fetchErrorMessage: payload };
+}
+
+const addUser = (state, payload) => {
+  const id = Math.max(...state.users.map(user => user.id)) + 1;
+  const newUser = { ...payload, id };
+  const newUsers = [...state, newUser];
+  return { ...state, users: newUsers };
 };
 
 const userReducer = (state = initialState, action) => {
   switch(action.type) {
-    case FETCH_USER_LOADING: {
-      return { ...state, isFetching: true }
-    }
-    case FETCH_USER_SUCCESS: {
-      return { ...state, users: action.payload, isFetching: false };
-    }
-    case ADD_USER_SUCCESS: {
-      const id = Math.max(...state.users.map(user => user.id)) + 1;
-      const newUser = { ...action.payload, id };
-      const newUsers = [...state, newUser];
-      return { ...state, users: newUsers };
-    }
+    case FETCH_USER_LOADING: return fetchUserStartLoading(state)
+    case FETCH_USER_SUCCESS: return fetchUser(state, action.payload)
+    case FETCH_USER_FAILED: return fetchUserFailed(state, action.payload)
+    case ADD_USER_SUCCESS: return addUser(state, action.payload)
     default: return state;
   }
 };
