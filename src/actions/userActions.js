44b66cbeb8eb177b9c99c34865a025296e1014f0
Adhy <adhywiranatap@gmail.com>
2017-05-01 22:58:34 +0700

    refactored user action creator

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,38 +1,30 @@
 import { FETCH_USER_SUCCESS, ADD_USER_SUCCESS } from './constants';
 
-export const addUserSuccess = (newUser) => {
-  return {
-    type: ADD_USER_SUCCESS,
-    payload: newUser,
-  };
+export const addUserSuccess = newUser => ({
+  type: ADD_USER_SUCCESS,
+  payload: newUser,
+});
+
+export const addUser = newUser => (dispatch) => {
+  fetch('http://localhost:1234/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newUser),
+  })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
 };
 
-export const addUser = (newUser) => {
-  return (dispatch) => {
-    fetch('http://localhost:1234/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(newUser),
-    })
-      .then(res => res.json())
-      .then(data => console.log(data))
-      .catch(err => console.log(err));
-  };
-};
+export const fetchUsersSuccess = users => ({
+  type: FETCH_USER_SUCCESS,
+  payload: users,
+});
 
-export const fetchUsersSuccess = (users) => {
-  return {
-    type: FETCH_USER_SUCCESS,
-    payload: users,
-  };
-};
-
-export const fetchUsers = () => {
-  return (dispatch) => {
-    fetch('http://localhost:1234/users')
-      .then(res => res.json())
-      .then(data => dispatch(fetchUsersSuccess(data)));
-  };
+export const fetchUsers = () => (dispatch) => {
+  fetch('http://localhost:1234/users')
+    .then(res => res.json())
+    .then(data => dispatch(fetchUsersSuccess(data)));
 };
