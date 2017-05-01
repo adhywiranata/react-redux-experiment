61025d84d25d3d48061852d952edcf3aedf78b43
Adhy <adhywiranatap@gmail.com>
2017-05-01 22:56:35 +0700

    refactored post action creators

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,39 +1,31 @@
 import { FETCH_POST_SUCCESS, ADD_POST_SUCCESS } from './constants';
 
-export const addPostSuccess = (newPost) => {
-  return {
-    type: ADD_POST_SUCCESS,
-    payload: newPost,
-  };
-};
+export const addPostSuccess = newPost => ({
+  type: ADD_POST_SUCCESS,
+  payload: newPost,
+});
 
-export const addPost = (newPost) => {
+export const addPost = newPost => (dispatch) => {
   newPost.createdAt = new Date().toISOString();
-  return (dispatch) => {
-    fetch('http://localhost:1234/posts', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(newPost),
-    })
-      .then(res => res.json())
-      .then(data => console.log(data))
-      .catch(err => console.log(err));
-  };
+  fetch('http://localhost:1234/posts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newPost),
+  })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
 };
 
-export const fetchPostsSuccess = (newUser) => {
-  return {
-    type: FETCH_POST_SUCCESS,
-    payload: newUser,
-  };
-};
+export const fetchPostsSuccess = newUser => ({
+  type: FETCH_POST_SUCCESS,
+  payload: newUser,
+});
 
-export const fetchPosts = () => {
-  return (dispatch) => {
-    fetch('http://localhost:1234/api/posts')
-      .then(res => res.json())
-      .then(data => dispatch(fetchPostsSuccess(data)));
+export const fetchPosts = () => (dispatch) => {
+  fetch('http://localhost:1234/api/posts')
+    .then(res => res.json())
+    .then(data => dispatch(fetchPostsSuccess(data)));
   };
-};
